fff300474dc71229198658406880851e812014d3
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-03-13 20:37:32 +0100

    Filled some food data

--- data/import_tool.php
+++ data/import_tool.php
@@ -1,5 +1,7 @@
 <?php
 
+// or use Chrome ext Copytables
+
 $s = '
 
 Energie	277 kcal
@@ -20,24 +22,23 @@ $s = str_ireplace('Fett',                        '  fat:          ', $s );  // s
 $s = str_ireplace('Kohlenhydrate',               '  carbs:        ', $s );
 $s = str_ireplace('davon Zucker',                '  sugar:        ', $s );
 $s = str_ireplace('Ballaststoffe',               '  fibre:        ', $s );
-$s = str_ireplace('Eiweiß',                      '  amino:       ', $s );
+$s = str_ireplace('Eiweiß',                      '  amino:        ', $s );
 $s = str_ireplace('Salz',                        '  salt:         ', $s );
 
 // preg_replace('/\s+/', ' ', $originalString);
 
 $s = str_replace(',', '.', $s );
 
-$s = str_replace(' g', '', $s );
-$s = str_replace(' mg', '', $s );
+$s = str_replace(' g',    '', $s );
+$s = str_replace(' mg',   '', $s );
 $s = str_replace(' kcal', '', $s );
-$s = str_replace(' kj', '', $s );
+$s = str_replace(' kj',   '', $s );
 
 echo "  unit:             \n"
    . "  weigth:           \n"
    . "$s\n"
    . "\n"
-   . "  STAT:             all\n"
    . "  sources:          Web\n"
-   . "  lastUpd:          2024-02-18\n"
+   . "  lastUpd:          2024-02-18\n";
 
 ?>
