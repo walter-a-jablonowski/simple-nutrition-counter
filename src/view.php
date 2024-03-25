d2265ed7a4547354dcd9d81e8b2abfe22f76778c
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-03-25 04:16:30 +0100

    Started extending UI

--- src/view.php
+++ src/view.php
@@ -29,7 +29,10 @@
         ><?= $this->data->dayEntriesTxt ?></textarea>
 
         <div class="row mt-2">  <!-- text-start text-left same for diff bs versions -->
-          <div class="col ml-3 pl-1 pr-1 bg-secondary text-start text-left small">
+          <div class="col ml-2 pl-1 pr-1 bg-secondary text-start text-left small">
+            <b>H2O</b>
+          </div>
+          <div class="col pl-1 pr-1 bg-secondary text-start text-left small">
             kcal
           </div>
           <div class="col pl-1 pr-1 border-left bg-secondary text-start text-left">
@@ -38,21 +41,26 @@
           <div class="col pl-1 pr-1 border-left bg-secondary text-start text-left">
             <div class="align-split small"><span>Pro</span> <span>g</span></div>
           </div>
-          <div class="col pl-1 mr-3 pr-1 border-left bg-secondary text-start text-left">
+          <div class="col pl-1 pr-1 border-left bg-secondary text-start text-left">
             <div class="align-split small"><span>Salt</span> <span>g</span></div>
           </div>
-          <div class="col text-end text-right" style="max-height: 20px;">
+          <div class="col pl-1 pr-1 border-left bg-secondary text-start text-left small">
+            <b>Price</b>
+          </div>
+          <div class="col mr-2 pl-1 text-end text-right" style="max-height: 20px;">
             <button class="btn btn-sm btn-light" onclick="foodsCrl.saveDayEntriesBtnClick(event)">
               Save
             </button>
           </div>
         </div>
         <div class="row">
-          <div id="caloriesSum" class="col ml-3 pl-1 pr-1 text-start text-left">0</div>
+          <div id="h2oSum" class="col ml-2 pl-1 pr-1 text-start text-left">0</div>
+          <div id="caloriesSum" class="col pl-1 pr-1 text-start text-left">0</div>
           <div id="fatSum" class="col pl-1 pr-1 text-end text-right">0</div>
           <div id="aminoSum" class="col pl-1 pr-1 text-end text-right">0</div>
-          <div id="saltSum" class="col pl-1 mr-3 pr-1 text-end text-right">0</div>
-          <div class="col text-end text-right">
+          <div id="saltSum" class="col pl-1 pr-1 text-end text-right">0</div>
+          <div id="priceSum" class="col pl-1 pr-1 text-end text-right">0</div>
+          <div class="col mr-2 pl-1 text-end text-right">
             &nbsp;
           </div>
         </div>
