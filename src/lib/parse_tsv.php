6812dfb1d729d6a2ac242e85bf5a530a4b9802cc
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-04-21 00:22:41 +0200

    Possible fix

--- src/lib/parse_tsv.php
+++ src/lib/parse_tsv.php
@@ -8,8 +8,9 @@ function parse_tsv( $entriesTxt )
   {
     $lines = explode("\n", $entriesTxt);
 
-    foreach( $lines as $line)
+    foreach( $lines as $line )
     {
+      // $line = trim($line);  // cause we are using str_pad() for amounts (looks like it's unneeded for some reason)
       $line = preg_replace('/ {2,}/', ';', trim($line));
       $entry = explode(';', $line);
       $foodEntries[] = $entry;
