6b954fe48847211b793477c4a7b3008e2ebf5069
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-04-24 11:43:04 +0200

    Added coffee conter design

--- src/view/tabs/edit/food_list_new.php
+++ src/view/tabs/edit/food_list_new.php
@@ -8,17 +8,40 @@
   <div class="col">
 
     <div class="row">
-      <div class="col-12" onclick = "...">
+      <div class="col-6" onclick = "...">
+        Enter ...
+      </div>
+      <div class="col-6" onclick = "...">
         Expired food ...
+      </div>
+    </div>
+
+    <!-- Coffee counter -->
+
+    <div class="row">
+      <div class="col">
+        Coffee
+      </div>
+      <div class="col" onclick = "...">
+        XXS
+      </div>
+      <div class="col" onclick = "...">
+        XS
+      </div>
+      <div class="col" onclick = "...">
+        M
+      </div>
+      <div class="col">
+        [VALUE GREEN|RED]
       </div>
     </div>
 
   </div>
 
   <?php
-  
+
   $done = [];
-  
+
   foreach( $layout as $idx => $def ):
 
     $type      = is_array( $def )   ?  'group' : 'single';
@@ -27,9 +50,9 @@
 
     $done[] = $name;  // left over will be printed below (done = foods and recipes n a single list)
   ?>
-    
+
     <?php if( $type == 'single'): ?>
-      // TASK
+      <!-- TASK -->
     <?php elseif( $type == 'group'): ?>
       <div class="col">
 
@@ -43,7 +66,7 @@
 
         <?php foreach( $entries as $name => $entry ): ?>
 
-          // TASK: we need name, amounts => nutirents in entry (merge on controller)
+          <!-- TASK: we need name, amounts => nutirents in entry (merge on controller) -->
 
           <div class="row">
             <div class="col p-2">
@@ -67,7 +90,7 @@
 
   <!-- left over entries -->
 
-  // TASK
+  <!-- TASK -->
 
   <?php if( count($foods) > count($done)): ?>
     <div class="col">
