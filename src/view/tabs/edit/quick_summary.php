48823e8315812b70654561d85b53bf6eb41cdb5e
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-04-26 15:13:31 +0200

    Minor fix

--- src/view/tabs/edit/quick_summary.php
+++ src/view/tabs/edit/quick_summary.php
@@ -1,5 +1,5 @@
 <div class="row mt-2">  <!-- text-start text-left same for diff bs versions -->
-  <div class="col ml-2 pl-1 pr-1 bg-secondary text-start text-left small">
+  <div class="col ms-2 pl-1 pr-1 bg-secondary text-start text-left small">
     <b>H2O</b>
   </div>
   <div class="col pl-1 pr-1 bg-secondary text-start text-left small">
@@ -14,15 +14,15 @@
   <div class="col pl-1 pr-1 border-left bg-secondary text-start text-left">
     <div class="align-split small"><span>Salt</span> <span>g</span></div>
   </div>
-  <div class="col pl-1 pr-1 border-left bg-secondary text-start text-left small">
+  <div class="col me-2 pl-1 border-left bg-secondary text-start text-left small">
     <b>Price</b>
   </div>
 </div>
 <div class="row">
-  <div id="h2oSum"      class="col ml-2 pl-1 pr-1 text-start text-left">0</div>
+  <div id="h2oSum"      class="col ms-2 pl-1 pr-1 text-start text-left">0</div>
   <div id="caloriesSum" class="col pl-1 pr-1 text-start text-left">0</div>
   <div id="fatSum"      class="col pl-1 pr-1 text-end text-right">0</div>
   <div id="aminoSum"    class="col pl-1 pr-1 text-end text-right">0</div>
   <div id="saltSum"     class="col pl-1 pr-1 text-end text-right">0</div>
-  <div id="priceSum"    class="col pl-1 pr-1 text-end text-right">0</div>
+  <div id="priceSum"    class="col me-2 pl-1 text-end text-right">0</div>
 </div>
