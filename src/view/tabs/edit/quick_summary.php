02300e962175056d56cab79b0ca761d64eaed1b0
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-05-02 00:39:44 +0200

    Nicer, fixed soime layout problems

--- src/view/tabs/edit/quick_summary.php
+++ src/view/tabs/edit/quick_summary.php
@@ -1,25 +1,25 @@
-<div class="row mt-2">
-  <div class="col data-badge ms-2 ps-0 pe-0" style="background-color: #FF6347;">
-    <div class="label-bg">kcal</div>
-	  <div id="caloriesSum" class="value-text">0</div>
+<div class="row mt-2 px-2">
+  <div class="col-2 ps-0 pe-0" style="background-color: #FF6347;">
+    <div class="ps-1 small">kcal</div>
+    <div id="caloriesSum" class="ps-1">0</div>
   </div>
-  <div class="col ps-1 pe-0">
-    <div class="ps-1 bg-secondary small">Carbs</div>
+  <div class="col-2 ps-1 pe-0">
+    <div class="ps-1 bg-secondary text-white small">Carbs</div>
     <div id="carbsSum" class="ps-1">0</div>
   </div>
-  <div class="col ps-1 pe-0">
-    <div class="px-1 align-split bg-secondary small"><span>Fat</span> <span>g</span></div>  <!-- align-split ai made class -->
+  <div class="col-2 ps-1 pe-0">
+    <div class="px-1 bg-secondary text-white small">Fat</div>  <!-- align-split ai made class -->
     <div id="fatSum" class="ps-1">0</div>
   </div>
-  <div class="col ps-1 pe-0">
-    <div class="px-1 align-split bg-secondary small"><span>Amino</span> <span>g</span></div>
+  <div class="col-2 ps-1 pe-0">
+    <div class="px-1 bg-secondary text-white small">Amino</div>
     <div id="aminoSum" class="ps-1">0</div>
   </div>
-  <div class="col ps-1 pe-0">
-    <div class="px-1 align-split bg-secondary small"><span>Salt</span> <span>g</span></div>
+  <div class="col-2 ps-1 pe-1">
+    <div class="px-1 bg-secondary text-white small">Salt</div>
     <div id="saltSum" class="ps-1">0</div>
   </div>
-  <div class="col data-badge me-2 ps-1 pe-0" style="background-color: #79fe28;">
+  <div class="col-2 data-badge ps-0 pe-0" style="background-color: #79fe28;">
     <div class="label-bg">&nbsp;</div>
 	  <div id="caloriesSum" class="value-text">
       <span>€</span>
