b7272a6f61b84f2909f47eccabaab1e5a31f8cac
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-05-02 00:48:12 +0200

    Added unit

--- src/view/tabs/edit/quick_summary.php
+++ src/view/tabs/edit/quick_summary.php
@@ -5,19 +5,19 @@
   </div>
   <div class="col-2 ps-1 pe-0">
     <div class="ps-1 bg-secondary text-white small">Carbs</div>
-    <div id="carbsSum" class="ps-1">0</div>
+    <div class="ps-1"><span id="carbsSum">0</span> g</div>
   </div>
   <div class="col-2 ps-1 pe-0">
-    <div class="px-1 bg-secondary text-white small">Fat</div>  <!-- align-split ai made class -->
-    <div id="fatSum" class="ps-1">0</div>
+    <div class="px-1 bg-secondary text-white small">Fat</div>
+    <div class="ps-1"><span id="fatSum">0</span> g</div>
   </div>
   <div class="col-2 ps-1 pe-0">
     <div class="px-1 bg-secondary text-white small">Amino</div>
-    <div id="aminoSum" class="ps-1">0</div>
+    <div class="ps-1"><span id="aminoSum">0</span> g</div>
   </div>
   <div class="col-2 ps-1 pe-1">
     <div class="px-1 bg-secondary text-white small">Salt</div>
-    <div id="saltSum" class="ps-1">0</div>
+    <div class="ps-1"><span id="saltSum">0</span> g</div>
   </div>
   <div class="col-2 ps-0 pe-0 d-flex justify-content-center align-items-center"
        style="background-color: #79fe28; font-size: 1.8rem;"
