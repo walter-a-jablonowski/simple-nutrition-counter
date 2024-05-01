e9d2a2cdb1530fc67f6f2e416ed5c7aad7d300f2
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-05-02 01:20:15 +0200

    Fixed html, added headline

--- src/view/tabs/last_days.php
+++ src/view/tabs/last_days.php
@@ -1,25 +1,30 @@
-<div class="row">
-  <div class="col scrollable-list">
+<div class="row ps-2 mb-2 small">
+  <div class="col-2 fw-bold">kcal</div>
+  <!-- <div class="col-2 fw-bold">Carbs/div> -->
+  <div class="col-2 fw-bold">Fat</div>
+  <div class="col-2 fw-bold">Amino</div>
+  <div class="col-2 fw-bold">Salt</div>
+</div>
+<div class="scrollable-list">
 
+  <div class="list-group">
     <?php foreach( $this->lastDaysSums as $day => $sums): ?>
-      <div class="list-group">
-        <a href="#" class="list-group-item list-group-item-action p-2">
-          <div class="d-flex w-100 justify-content-between">
-            <?php $weekdays = ['Mon' => 'Mo', 'Tue' => 'Tu', 'Wed' => 'We', 'Thu' => 'Th', 'Fri' => 'Fr', 'Sat' => 'Sa', 'Sun' => 'Su']; ?>
-            <small class="text-body-secondary mb-1"><b><?= $weekdays[ date('D', strtotime($day))] ?></b>
-            &nbsp;<?= $day ?></small>
-            <small class="text-body-secondary mb-1"><b><?= $sums['priceSum'] ?> EUR</b></small>
-          </div>
-          <div class="row small">
-            <div class="col-2"><?= $sums['caloriesSum'] ?> kcal</div>
-            <!-- <div class="col-2">< ?= $sums['carbsSum'] ?> g</div> -->
-            <div class="col-2"><?= $sums['fatSum'] ?> g</div>
-            <div class="col-2"><?= $sums['aminoSum'] ?> g</div>
-            <div class="col-2"><?= $sums['saltSum'] ?> g</div>
-          </div>
-        </a>
-      </div>
+      <a href="#" class="list-group-item list-group-item-action p-2">
+        <div class="d-flex w-100 justify-content-between">
+          <?php $weekdays = ['Mon' => 'Mo', 'Tue' => 'Tu', 'Wed' => 'We', 'Thu' => 'Th', 'Fri' => 'Fr', 'Sat' => 'Sa', 'Sun' => 'Su']; ?>
+          <small class="text-body-secondary mb-1"><b><?= $weekdays[ date('D', strtotime($day))] ?></b>
+          &nbsp;<?= $day ?></small>
+          <small class="text-body-secondary mb-1"><b><?= $sums['priceSum'] ?> EUR</b></small>
+        </div>
+        <div class="row small">
+          <div class="col-2"><?= $sums['caloriesSum'] ?> kcal</div>
+          <!-- <div class="col-2">< ?= $sums['carbsSum'] ?> g</div> -->
+          <div class="col-2"><?= $sums['fatSum'] ?> g</div>
+          <div class="col-2"><?= $sums['aminoSum'] ?> g</div>
+          <div class="col-2"><?= $sums['saltSum'] ?> g</div>
+        </div>
+      </a>
     <?php endforeach; ?>
-
   </div>
+
 </div>
