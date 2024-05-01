bb564d919d24daab96615b80747746a3b8ae316d
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-05-02 00:45:19 +0200

    Simplified

--- src/view/tabs/edit/quick_summary.php
+++ src/view/tabs/edit/quick_summary.php
@@ -1,6 +1,6 @@
 <div class="row mt-2 px-2">
   <div class="col-2 ps-0 pe-0" style="background-color: #FF6347;">
-    <div class="ps-1 small">kcal</div>
+    <div class="ps-1 fw-bold small">kcal</div>
     <div id="caloriesSum" class="ps-1">0</div>
   </div>
   <div class="col-2 ps-1 pe-0">
@@ -19,11 +19,9 @@
     <div class="px-1 bg-secondary text-white small">Salt</div>
     <div id="saltSum" class="ps-1">0</div>
   </div>
-  <div class="col-2 data-badge ps-0 pe-0" style="background-color: #79fe28;">
-    <div class="label-bg">&nbsp;</div>
-	  <div id="caloriesSum" class="value-text">
-      <span>€</span>
-      <span id="priceSum">0</span>
-    </div>
+  <div class="col-2 ps-0 pe-0 d-flex justify-content-center align-items-center"
+       style="background-color: #79fe28; font-size: 1.8rem;"
+  >
+    &euro; <span id="priceSum">0</span>
   </div>
 </div>
