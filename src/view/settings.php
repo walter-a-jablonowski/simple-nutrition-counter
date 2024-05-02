a08fce114ef6a70f375e5cd7333969e2777202e0
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-05-02 21:41:39 +0200

    Extrend settings menu

--- src/view/settings.php
+++ src/view/settings.php
@@ -1,12 +1,41 @@
 <ul class="list-group">
-  <li class="list-group-item" onclick="" style="cursor: pointer;">
-    Edit foods (advanced feature)
+  <li class="list-group-item">
+    <span class="fw-bold">Edit</span> (advanced feature)
   </li>
-  <li class="list-group-item" onclick="foodsCrl.helpBtn(event)" style="cursor: pointer;">
+  <li class="list-group-item" onclick="" style="cursor: pointer;">
+    Foods
+  </li>
+  <li class="list-group-item" onclick="" style="cursor: pointer;">
+    Recipes
+  </li>
+  <li class="list-group-item" onclick="" style="cursor: pointer;">
+    Layout
+  </li>
+<!--
+  <li class="list-group-item" onclick="" style="cursor: pointer;">
+    Misc
+  </li>
+-->
+  <li class="list-group-item" onclick="" style="cursor: pointer;">
+    Expenses
+  </li>
+  <li class="list-group-item" onclick="" style="cursor: pointer;">
+    Misc counters
+  </li>
+  <li class="list-group-item" onclick="" style="cursor: pointer;">
+    Nutrients
+  </li>
+  <li class="list-group-item" onclick="" style="cursor: pointer;">
+    Nutrient groups
+  </li>
+</ul>
+
+<ul class="list-group mt-3">
+  <li class="list-group-item fw-bold" onclick="foodsCrl.helpBtn(event)" style="cursor: pointer;">
     Help
   </li>
 </ul>
 
-<button onclick="foodsCrl.settingsBtnClick(event)" class="btn btn-sm btn-primary mt-2">
+<button onclick="foodsCrl.settingsBtnClick(event)" class="btn btn-sm btn-primary mt-3">
   Done
 </button>
