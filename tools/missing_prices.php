3dac535b45474a3944fc27ec023c1f82a33e3ea1
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-05-02 14:30:52 +0200

    Added tool

---
+++ tools/missing_prices.php
@@ -0,0 +1,38 @@
+<?php
+
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Exception\ParseException;
+
+chdir('../src');
+
+require_once 'vendor/autoload.php';
+
+
+// Look for missing prices and old prices
+
+define('OLD_PRICE', 6 * 30);
+
+
+$foods = Yaml::parse( file_get_contents('data/foods.yml'));
+$r = [];
+
+foreach( $foods as $key => $food)
+{
+  $lastPriceUpdate = isset($food['lastPriceUpd']) && is_numeric($food['lastPriceUpd'])
+                  ? (new DateTime())->setTimestamp($food['lastPriceUpd'])
+                  : new DateTime();
+
+  $diff = (new DateTime())->diff($lastPriceUpdate);
+
+  if( ! $food['price'] || $diff->days > OLD_PRICE )
+  {
+    $r[$key] = [
+      'price' => $food['price'],
+      'lastPriceUpd' => $food['lastPriceUpd']
+    ];
+  }
+}
+
+print_r($r);
+
+?>
