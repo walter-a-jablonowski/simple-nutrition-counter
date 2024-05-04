766405180fb0976abaad79c9dce3b88bb391cb53
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-05-04 22:06:01 +0200

    Minor fix

--- src/view/error_page.php
+++ src/view/error_page.php
@@ -1,7 +1,7 @@
 <div class="d-flex align-items-center justify-content-center ext-page-container">
   <div class="text-center ext-round-box">
     <div class="ext-icon-box">
-      <img src="../style/ladybug.png" alt="Error Image" style="width: 200px; height: auto;">
+      <img src="style/ladybug.png" alt="Error Image" style="width: 200px; height: auto;">
     </div>
     <h1 class="ext-heading">We have a bug</h1>
     <p class="fs-5">We're sorry for the inconvenience. Please try again later.</p>
