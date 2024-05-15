15b570bcaabfce9024fbe9a4bd47ee680226fd33
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-05-15 23:09:50 +0200

    Removed quick summary design for now

--- src/view/tabs/edit/quick_summary.php
+++ src/view/tabs/edit/quick_summary.php
@@ -3,16 +3,17 @@
     <div class="ps-1 fw-bold small">kcal</div>
     <div id="caloriesSum" class="ps-1">0</div>
   </div>
-<!--
   <div class="col-2 ps-1 pe-0">
     <div class="ps-1 bg-secondary text-white small">Carbs</div>
     <div class="ps-1"><span id="carbsSum">0</span> g</div>
   </div>
--->
+  <!-- (TASK) space saving quick summary design -->
+<!-- 
   <div class="col-2 val">
     <div class="label">Carbs</div>
     <div class="value"><span id="carbsSum">0</span> g</div>
   </div>
+-->
   <div class="col-2 ps-1 pe-0">
     <div class="px-1 bg-secondary text-white small">Fat</div>
     <div class="ps-1"><span id="fatSum">0</span> g</div>
