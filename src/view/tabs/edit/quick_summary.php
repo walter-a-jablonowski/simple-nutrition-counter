f26f320612458df5607a8fdc1929be54fbabfdf5
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-05-15 23:24:01 +0200

    Trying

--- src/view/tabs/edit/quick_summary.php
+++ src/view/tabs/edit/quick_summary.php
@@ -1,11 +1,11 @@
-<div class="row mt-2 px-2">
+<div id="#quickSummary" class="row mt-2 px-2">
   <div class="col-2 ps-0 pe-0" style="background-color: #eb6864;">
     <div class="ps-1 fw-bold small">kcal</div>
     <div id="caloriesSum" class="ps-1">0</div>
   </div>
   <div class="col-2 ps-1 pe-0">
     <div class="ps-1 bg-secondary text-white small">Carbs</div>
-    <div class="ps-1"><span id="carbsSum">0</span> g</div>
+    <div class="value ps-1"><span id="carbsSum">0</span> g</div>
   </div>
   <!-- (TASK) space saving quick summary design -->
 <!-- 
@@ -16,15 +16,15 @@
 -->
   <div class="col-2 ps-1 pe-0">
     <div class="px-1 bg-secondary text-white small">Fat</div>
-    <div class="ps-1"><span id="fatSum">0</span> g</div>
+    <div class="value ps-1"><span id="fatSum">0</span> g</div>
   </div>
   <div class="col-2 ps-1 pe-0">
     <div class="px-1 bg-secondary text-white small">Amino</div>
-    <div class="ps-1"><span id="aminoSum">0</span> g</div>
+    <div class="value ps-1"><span id="aminoSum">0</span> g</div>
   </div>
   <div class="col-2 ps-1 pe-1">
     <div class="px-1 bg-secondary text-white small">Salt</div>
-    <div class="ps-1"><span id="saltSum">0</span> g</div>
+    <div class="value ps-1"><span id="saltSum">0</span> g</div>
   </div>
   <div class="col-2 ps-0 pe-0 d-flex justify-content-center align-items-center"
        style="background-color: #79fe28; font-size: 1.8rem;"
