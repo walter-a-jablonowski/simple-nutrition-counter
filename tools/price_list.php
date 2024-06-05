422c0b35df8c746dc60c9239374f239bc470058d
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-06-05 06:01:21 +0200

    Minor fix

--- tools/price_list.php
+++ tools/price_list.php
@@ -18,7 +18,10 @@ foreach( $foods as $food => $data )
 
   // $last_price_upd = ! $data['lastPriceUpd'] ? null : DateTime::createFromFormat('Y-m-d', $data['lastPriceUpd']);
   // $minDateObj     = ! $min_date ? null : DateTime::createFromFormat('Y-m-d', $min_date);
-  
+
+  if( $food == 'Riegel Deca Basic')
+    $debug = 'halt';
+
   if( is_null($data['lastPriceUpd']) || ( $min_date === null || $data['lastPriceUpd'] >= $min_date ))
     $r[ $data['vendor']][] = [
       'food'  => $food,
@@ -29,12 +32,12 @@ foreach( $foods as $food => $data )
 
 $output = '';
 
-foreach( $r as $vendor => $foods )  // TASK: missing prices still displayed as 0.00
+foreach( $r as $vendor => $foods )  
 {
   $output .= "\n$vendor\n\n";
   
-  foreach( $foods as $entry )
-    $output .= sprintf("%-23s  %-6.2f  %-10s\n", $entry['food'], $entry['price'] ?: '', $entry['lastPriceUpd']);
+  foreach( $foods as $entry )  // TASK: looks like formatting problem if Umlaut in name
+    $output .= str_pad( $entry['food'], 26) . str_pad( $entry['price'], 5, ' ', STR_PAD_LEFT) . "  $entry[lastPriceUpd]\n";
 }
 
 file_put_contents( $out_file, $output );
