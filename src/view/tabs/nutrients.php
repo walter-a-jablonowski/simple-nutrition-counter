961be2bb08c520736d27aa73ad46b2ee67e81eb1
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-06-09 10:09:04 +0200

    Added collapse

--- src/view/tabs/nutrients.php
+++ src/view/tabs/nutrients.php
@@ -1,22 +1,35 @@
 
 <div class="scrollable-list">
-  <div id="">
-    <?php foreach( $this->model->nutrients as $groupShort => $group ): ?>
-      <?php // TASK: add collapse ?>
-      <?php foreach( $group as $short => $data ): ?>
-        <div class      = "nutrients-entry mb-2"
-            data-group = "<?= $groupShort ?>"
-            data-short = "<?= $short ?>"
-            data-ideal = "<?= $data['ideal'] ?>"
-        >
-          <div><?= $data['name'] ?></div>  <!-- TASK: or right align 80 / 100 -->
-          <div class="progress w-100" role="progressbar">
-            <div class="progress-bar bg-success" style="width: 0%;">
-              <span><!-- < ?= $data['name'] ?> --><span class="progress-label">0 / <?= $val['ideal'] ?></span></span>
+
+  <?php foreach( $this->model->nutrients as $groupShort => $group ): ?>
+
+    <ul class="list-group">  <!-- TASK: alternative grid ins of list group -->
+
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <a data-bs-toggle="collapse" href="#<?= $groupShort ?>Collapse" role="button">
+          <?= $groupShort ?>  <!-- TASK: use collapse on whole elem ? -->
+        </a>
+        <span>v</span>        <!-- TASK -->
+      </li>
+      <li id="<?= $groupShort ?>Collapse" class="list-group-item collapse show">
+
+        <?php foreach( $group as $short => $data ): ?>
+
+          <div class     = "nutrients-entry mb-2"
+              data-group = "<?= $groupShort ?>"
+              data-short = "<?= $short ?>"
+              data-ideal = "<?= $data['ideal'] ?>"
+          >
+            <div><?= $data['name'] ?></div>  <!-- TASK: or right align 80 / 100 -->
+            <div class="progress w-100" role="progressbar">
+              <div class="progress-bar bg-success" style="width: 0%;">
+                <span><!-- < ?= $data['name'] ?> --><span class="progress-label">0 / <?= $data['ideal'] ?></span></span>
+              </div>
             </div>
           </div>
-        </div>
-      <?php endforeach; ?>
-    <?php endforeach; ?>
-  </div>
+        <?php endforeach; ?>
+
+      </li>
+    </ul>
+  <?php endforeach; ?>
 </div>
