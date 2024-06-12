47e01e262039dd0affe5d3713e533e3367f3f3e1
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-06-12 13:33:32 +0200

    Some fix for aligning the headlines

--- src/view/tabs/edit/food_list.php
+++ src/view/tabs/edit/food_list.php
@@ -41,7 +41,7 @@
     <div class="col-12 col-md-6 col-lg-4 col-xxl-3">
 
       <div class="row">
-        <div class = "col-12 p-1 small fw-bold d-flex justify-content-between align-items-center"
+        <div class = "col-12 ms-1 p-1 pe-2 small fw-bold d-flex justify-content-between align-items-center"
              style = "background-color: <?= $def['@attribs']['color'] ?? '#e0e0e0' ?>;"
         >
           <?= $groupName ?>
@@ -106,7 +106,7 @@
     <div class="col-12 col-md-6 col-lg-4 col-xxl-3">
 
       <div class="row">
-        <div class = "col-12 p-1 small fw-bold d-flex justify-content-between align-items-center"
+        <div class = "col-12 p-1 ps-2 pe-2 small fw-bold d-flex justify-content-between align-items-center"
              style = "background-color: #e0e0e0;"
         >
           Misc foods
