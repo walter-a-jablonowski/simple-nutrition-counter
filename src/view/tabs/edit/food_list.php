5159531dc19ede60e15d76ef173a9f18c9577242
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-06-12 13:27:33 +0200

    Fixed responsive

--- src/view/tabs/edit/food_list.php
+++ src/view/tabs/edit/food_list.php
@@ -38,7 +38,7 @@
     
     $collapseId = str_replace(' ', '', $groupName);
   ?>
-    <div class="col-4">
+    <div class="col-12 col-md-6 col-lg-4 col-xxl-3">
 
       <div class="row">
         <div class = "col-12 p-1 small fw-bold d-flex justify-content-between align-items-center"
@@ -103,7 +103,7 @@
   if( $all > count($done)):
   
   ?>
-    <div class="col-4">
+    <div class="col-12 col-md-6 col-lg-4 col-xxl-3">
 
       <div class="row">
         <div class = "col-12 p-1 small fw-bold d-flex justify-content-between align-items-center"
