8fcb4f6f81b6cbd19a2249808e4d0b92da7cf4f6
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-06-21 13:46:33 +0200

    Fixed icon

--- src/view/tabs/edit/food_list/food_info_headline.php
+++ src/view/tabs/edit/food_list/food_info_headline.php
@@ -1,22 +1,20 @@
 <!-- TASK: (advanced) sometimes it isn't the vendor url, no better place for url for now -->
 
-<div class="mb-1 fw-bold d-flex justify-content-between align-items-center">
-  <span>
-    <?= htmlspecialchars($foodName) ?>
-    <?php if( ! empty($data['vendor']) || ! empty($data['url'])): ?>
-      <span class="fw-normal small">
-        <?php if( ! empty($data['vendor']) && ! empty($data['url'])): ?>
-          (<a href="<?= $data['url'] ?>" target="_blank" class="text-decoration-none"><?= $data['vendor'] ?></a>)
-        <?php elseif( empty($data['vendor']) && ! empty($data['url'])): ?>
-          (<a href="<?= $data['url'] ?>" target="_blank" class="text-decoration-none">url</a>)
-        <?php elseif( ! empty($data['vendor']) && empty($data['url'])): ?>
-          (<?= $data['vendor'] ?>)
-        <?php endif; ?>
-      </span>
-    <?php endif; ?>
-  </span>
+<div class="mb-1 fw-bold">
+  <?= htmlspecialchars($foodName) ?>
+  <?php if( ! empty($data['vendor']) || ! empty($data['url'])): ?>
+    <span class="fw-normal small">
+      <?php if( ! empty($data['vendor']) && ! empty($data['url'])): ?>
+        (<a href="<?= $data['url'] ?>" target="_blank" class="text-decoration-none"><?= $data['vendor'] ?></a>)
+      <?php elseif( empty($data['vendor']) && ! empty($data['url'])): ?>
+        (<a href="<?= $data['url'] ?>" target="_blank" class="text-decoration-none">url</a>)
+      <?php elseif( ! empty($data['vendor']) && empty($data['url'])): ?>
+        (<?= $data['vendor'] ?>)
+      <?php endif; ?>
+    </span>
+  <?php endif; ?>
   <?php if( $this->devMode ): ?>
-    <i class="bi bi-pencil-square text-black"></i>  <!-- TASK: (advanced) or make all editable on typ -->
+    &nbsp;&nbsp;&nbsp;<i class="bi bi-pencil-square text-black"></i>  <!-- TASK: (advanced) or make all editable on typ -->
   <?php endif; ?>
 </div>
 
