b388e3eb90f66fd99c62fb65b548af59a4accaf6
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-06-21 13:43:57 +0200

    Moved badges

--- src/view/tabs/edit/food_list/food_info.php
+++ src/view/tabs/edit/food_list/food_info.php
@@ -10,49 +10,6 @@ $nutrientsShort = [
 ];
 
 ?>
-<!-- Badges -->
-<!-- TASK: add { oekotest: "sehr gut" } -->
-
-<?php if( ! empty($data['acceptable'])): ?>
-  <span class="badge bg-<?= $this->iif( $data['acceptable'] == 'less', 'danger', 'warning') ?>">
-    <?= $this->iif( $data['acceptable'] == 'less', 'less good', 'occasionally') ?>
-  </span>
-<?php endif; ?>
-<span class="badge bg-<?= $this->iif( ! empty($data['properties']['bio']), 'success', 'secondary') ?>">
-  <?= $this->iif( ! empty($data['properties']['bio']), 'bio', '<s>bio</s>') ?>  <!-- TASK: non working -->
-</span>
-<span class="badge bg-<?= $this->iif( ! empty($data['properties']['vegan']), 'success', 'secondary') ?>">
-  <?= $this->iif( ! empty($data['properties']['vegan']), 'vegan', '<s>vegan</s>') ?>
-</span>
-<?php if( ! empty($data['properties']['NutriScore'])): ?>
-  <span class="badge bg-info">NutriScore</span>
-<?php endif; ?>
-
-<!-- High fat ... -->
-<!-- TASK: add -->
-
-<?php if( $data['nutritionalValues']['fat'] > config::get('highIntake.fat')): ?>
-  <span class="badge bg-danger">fatty</span>
-<?php endif; ?>
-
-<!-- Gluten and similar from ingredients list -->
-<!-- TASK: maybe also add a flag gluten: true in food data where you can add it manually -->
-<!-- TASK: (advanced) add high calcium ... -->
-
-<?php if( ! empty($data['ingredients'])): ?>
-  <?php foreach( config::get('substances.gluten') as $s ): ?>
-    <?php if( stripos( $data['ingredients'], $s) !== false): ?>
-      <span class="badge bg-danger">gluten</span>
-    <?php break; endif; ?>
-  <?php endforeach; ?>
-<?php endif; ?>
-<?php if( ! empty($data['ingredients'])): ?>
-  <?php foreach( config::get('substances.lactose') as $s ): ?>
-    <?php if( stripos( $data['ingredients'], $s) !== false): ?>
-      <span class="badge bg-danger">lactose</span>
-    <?php break; endif; ?>
-  <?php endforeach; ?>
-<?php endif; ?>
 
 <!-- Comment -->
 
