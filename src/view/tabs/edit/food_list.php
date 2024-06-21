371aa0ddad996cbb0d96f6d18e57dbd2ea288930
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-06-21 11:53:58 +0200

    Minor fix

--- src/view/tabs/edit/food_list.php
+++ src/view/tabs/edit/food_list.php
@@ -21,8 +21,8 @@ Structure overview
       <div class   = "col-12 col-md-6 col-lg-4 col-xxl-3 p-1"
            onclick = "foodsCrl.newEntryBtn(event)"
       >
-        Enter manually ...  <!-- TASK: also add month var and buyings here (maybe use some select that changes sub forms) -->
-      </div>
+        Enter manually ...  <!-- TASK: also buyings here (maybe use some select that changes sub forms) -->
+      </div>                <!-- month var: we have a layout group for this -->
       <!-- TASK: currently used save btn -->
       <div class   = "col-12 col-md-6 col-lg-4 col-xxl-3 p-1"
            onclick = "foodsCrl.saveDayEntriesBtnClick(event)"
