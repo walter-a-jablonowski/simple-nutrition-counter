b3391478b71683a8b4df19d628ce350d0afcde5f
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-06-28 20:05:45 +0200

    Trying font sizes

--- src/view/tabs/edit/quick_summary.php
+++ src/view/tabs/edit/quick_summary.php
@@ -2,12 +2,12 @@
 <div id="#quickSummary" class="row px-2">
   <div class="col-6 col-md-4 col-xxl-2 mt-2 px-1">
     <div style="background-color: #eb6864;">
-      <div class="ps-1 fw-bold small">kcal</div>
+      <div class="ps-1 fw-bold">kcal</div>
       <div id="caloriesSum" class="ps-1">0</div>
     </div>
   </div>
   <div class="col-6 col-md-4 col-xxl-2 mt-2 px-1">
-    <div class="ps-1 bg-secondary text-white small">Carbs</div>
+    <div class="ps-1 bg-secondary text-white">Carbs</div>
     <div class="value ps-1"><span id="carbsSum">0</span> g</div>
   </div>
   <!-- (TASK) space saving quick summary design -->
@@ -18,15 +18,15 @@
   </div>
 -->
   <div class="col-6 col-md-4 col-xxl-2 mt-2 px-1">
-    <div class="px-1 bg-secondary text-white small">Fat</div>
+    <div class="px-1 bg-secondary text-white">Fat</div>
     <div class="value ps-1"><span id="fatSum">0</span> g</div>
   </div>
   <div class="col-6 col-md-4 col-xxl-2 mt-2 px-1">
-    <div class="px-1 bg-secondary text-white small">Amino</div>
+    <div class="px-1 bg-secondary text-white">Amino</div>
     <div class="value ps-1"><span id="aminoSum">0</span> g</div>
   </div>
   <div class="col-6 col-md-4 col-xxl-2 mt-2 px-1">
-    <div class="px-1 bg-secondary text-white small">Salt</div>
+    <div class="px-1 bg-secondary text-white">Salt</div>
     <div class="value ps-1"><span id="saltSum">0</span> g</div>
   </div>
   <div class="col-6 col-md-4 col-xxl-2 mt-2 px-1">
