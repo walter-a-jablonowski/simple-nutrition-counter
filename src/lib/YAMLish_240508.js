f9bbd38f67ceef099780b5fd44bffcc24823d422
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-07-05 14:16:27 +0200

    Minor task

--- src/lib/YAMLish_240508.js
+++ src/lib/YAMLish_240508.js
@@ -10,7 +10,7 @@ class YAMLish
     return JSON.stringify(obj)
       .replace(/"(\w+)":/g, '$1:')     // no " for keys
       .replace(/(,|:)(?!\s)/g, '$1 ')  // colon or comma followed by a space of none (Symfony yaml requirement)
-      .replace('[]', '{}')
+      .replace('[]', '{}')             // TASK: is this a good idea here?
       // .replace(/(,|:)/g, '$1 ')     //   neg look head no space
       // .replace(/\s+/g, ' ')         // single space only
   }
