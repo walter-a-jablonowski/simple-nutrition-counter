9438feb4d0924a7b7bcd7c6a08ea8e15a003d22b
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-07-20 18:11:21 +0200

    Added food

--- src/controller.js
+++ src/controller.js
@@ -37,8 +37,8 @@ class FoodsEventController
 
     // this.popoverTriggerList = [].slice.call( document.querySelectorAll('[data-bs-toggle="popover"]'))
     this.popoverTriggerList = [].slice.call( query('[data-bs-toggle="popover"]'))
-    this.popoverList = this.popoverTriggerList.map( function (popoverTriggerEl) {
-      return new bootstrap.Popover(popoverTriggerEl, {
+    this.popoverList = this.popoverTriggerList.map( function( popoverTriggerEl) {
+      return new bootstrap.Popover( popoverTriggerEl, {
         html: true,
         customClass: 'popover-cus'
       })
