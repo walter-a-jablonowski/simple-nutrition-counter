51772ad6c359aa5f13c018a7802541f0b01985dd
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-07-21 13:40:28 +0200

    Make tab 3 more similar second tab

--- src/view/tabs/last_days.php
+++ src/view/tabs/last_days.php
@@ -12,6 +12,7 @@
   </li>
 </ul>
 
+<!--
 <div class="row ps-2 mt-4 mb-2 small">
   <div class="col-2 fw-bold">kcal</div>
   <div class="col-2 fw-bold">Carbs</div>
@@ -19,26 +20,50 @@
   <div class="col-2 fw-bold">Amino</div>
   <div class="col-2 fw-bold">Salt</div>
 </div>
-<div class="scrollable-list">
+-->
 
+<div class="scrollable-list">
+<!--
   <div class="list-group">
-    <?php foreach( $this->lastDaysSums as $day => $sums): ?>
+    < ?php foreach( $this->lastDaysSums as $day => $sums): ?>
       <a href="#" class="list-group-item list-group-item-action p-2">
         <div class="d-flex w-100 justify-content-between">
-          <?php $weekdays = ['Mon' => 'Mo', 'Tue' => 'Tu', 'Wed' => 'We', 'Thu' => 'Th', 'Fri' => 'Fr', 'Sat' => 'Sa', 'Sun' => 'Su']; ?>
-          <small class="text-body-secondary mb-1"><b><?= $weekdays[ date('D', strtotime($day))] ?></b>
-          &nbsp;<?= $day ?></small>
-          <small class="text-body-secondary mb-1"><b><?= $sums['priceSum'] ?> EUR</b></small>
+          < ?php $weekdays = ['Mon' => 'Mo', 'Tue' => 'Tu', 'Wed' => 'We', 'Thu' => 'Th', 'Fri' => 'Fr', 'Sat' => 'Sa', 'Sun' => 'Su']; ?>
+          <small class="text-body-secondary mb-1"><b>< ?= $weekdays[ date('D', strtotime($day))] ?></b>
+          &nbsp;< ?= $day ?></small>
+          <small class="text-body-secondary mb-1"><b>< ?= $sums['priceSum'] ?> EUR</b></small>
         </div>
         <div class="row small">
-          <div class="col-2"><?= $sums['caloriesSum'] ?> kcal</div>
-          <div class="col-2"><?= $sums['carbsSum'] ?> g</div>
-          <div class="col-2"><?= $sums['fatSum'] ?> g</div>
-          <div class="col-2"><?= $sums['aminoSum'] ?> g</div>
-          <div class="col-2"><?= $sums['saltSum'] ?> g</div>
+          <div class="col-2">< ?= $sums['caloriesSum'] ?> kcal</div>
+          <div class="col-2">< ?= $sums['carbsSum'] ?> g</div>
+          <div class="col-2">< ?= $sums['fatSum'] ?> g</div>
+          <div class="col-2">< ?= $sums['aminoSum'] ?> g</div>
+          <div class="col-2">< ?= $sums['saltSum'] ?> g</div>
         </div>
       </a>
+    < ?php endforeach; ?>
+  </div>
+-->
+  <div class="list-group">
+
+    <?php foreach( $this->lastDaysSums as $day => $sums): ?>
+
+      <li class = "list-group-item d-flex justify-content-between align-items-center"
+          style = "background-color: #e0e0e0;"
+      >
+        <?php $weekdays = ['Mon' => 'Mo', 'Tue' => 'Tu', 'Wed' => 'We', 'Thu' => 'Th', 'Fri' => 'Fr', 'Sat' => 'Sa', 'Sun' => 'Su']; ?>
+        <small class="text-body-secondary mb-1"><b><?= $weekdays[ date('D', strtotime($day))] ?></b>
+        &nbsp;<?= $day ?></small>
+        <small class="text-body-secondary mb-1"><b><?= $sums['priceSum'] ?> EUR</b></small>
+      </li>
+
+      <?php foreach(['caloriesSum', 'fatSum', 'carbsSum', 'aminoSum', 'saltSum'] as $type): ?>
+
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          <span><?= $type ?></span>
+          <span><?= $sums[$type] ?></span>  <!-- TASK: name, kcal or g -->
+        </li>
+      <?php endforeach; ?>
     <?php endforeach; ?>
   </div>
-
 </div>
