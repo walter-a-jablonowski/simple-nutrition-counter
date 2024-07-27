f3921a9c2643dd533450d9f1d43c8c88f8af2e97
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-07-27 18:08:21 +0200

    Smaller alternative

--- src/view/tabs/edit/food_list/group.php
+++ src/view/tabs/edit/food_list/group.php
@@ -84,7 +84,13 @@ $return['done'] = [];
               data-source    = "#<?= $foodId ?>Data"
         >
           <?= $foodName ?>
-          <?= self::iif( $showInfo,        '<span class="badge" style="background-color: orange; padding: 2px 5px 4px 5px !important;">info</span>') ?>
+          <!-- < ?= self::iif( $showInfo, '<span class="badge" style="background-color: orange; padding: 2px 5px 4px 5px !important;">info</span>') ?> -->
+          <?= self::iif( $showInfo,
+            // bi bi-info-circle-fill
+            '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="orange" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
+              <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
+            </svg>'
+          ) ?>
           <?= self::iif( $price && $cheap, '<i class="bi bi-currency-exchange small text-secondary"></i>') ?>
           <!-- TASK: use &euro; or the icon in config -->
           <?= self::iif( $price && $expensive, self::switch( settings::get('currency'), [
