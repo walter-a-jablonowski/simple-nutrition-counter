ceb6655488ec9793b78a2e6f3fd293f8c15915d0
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-07-28 15:53:03 +0200

    Modified quick summary

--- src/view/tabs/edit/quick_summary.php
+++ src/view/tabs/edit/quick_summary.php
@@ -7,8 +7,8 @@
     </div>
   </div>
   <div class="col-6 col-md-2 col-xxl-2 mt-2 px-1">
-    <div class="ps-1 bg-secondary text-white">Fat / Carbs</div>
-    <div class="value ps-1"><span id="carbsSum">0</span> / <span id="fatSum">0</span> g</div>
+    <div class="ps-1 bg-secondary text-white">Fat / Amino (2:1)</div>
+    <div class="value ps-1"><span id="fatSum">0</span> / <span id="aminoSum">0</span> g</div>
   </div>
   <!-- (TASK) space saving quick summary design -->
 <!-- 
@@ -18,15 +18,15 @@
   </div>
 -->
   <div class="col-6 col-md-2 col-xxl-2 mt-2 px-1">
-    <div class="px-1 bg-secondary text-white">Amino</div>
-    <div class="value ps-1"><span id="aminoSum">0</span> g</div>
+    <div class="px-1 bg-secondary text-white">Carbs &le; 50g</div>
+    <div class="value ps-1"><span id="carbsSum">0</span> g</div>
   </div>
   <div class="col-6 col-md-2 col-xxl-2 mt-2 px-1">
-    <div class="px-1 bg-secondary text-white">Fibre</div>
+    <div class="px-1 bg-secondary text-white">Fibre ?</div>
     <div class="value ps-1"><!-- <span id="fibreSum">0</span> --> g</div>
   </div>
   <div class="col-6 col-md-2 col-xxl-2 mt-2 px-1">
-    <div class="px-1 bg-secondary text-white">Salt</div>
+    <div class="px-1 bg-secondary text-white">Salt 4-6g</div>
     <div class="value ps-1"><span id="saltSum">0</span> g</div>
   </div>
   <div class="col-6 col-md-2 col-xxl-2 mt-2 px-1">
