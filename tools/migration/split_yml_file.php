43f7948056dc8dcc2ce52164164edd6acc455856
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-08-26 12:25:01 +0200

    Minor change

--- tools/migration/split_yml_file.php
+++ tools/migration/split_yml_file.php
@@ -21,8 +21,8 @@ Use scandir(). Ident all codes wit 2 spaces.
 
 */
 
-$sourceFile = 'path/to/your/source.yml';
-$destFolder = 'path/to/destination/folder';
+$sourceFile = 'src/data/bundles/Default_JaneDoe@example.com-24080101000000/foods/-this.yml';
+$destFolder = 'src/data/bundles/Default_JaneDoe@example.com-24080101000000/foods';
 
 try {
 
