0d84ec79a761920987e3c13a5e90454310434967
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-09-27 02:22:35 +0200

    Minor change

--- tools/deploy/deploy.php
+++ tools/deploy/deploy.php
@@ -1,6 +1,7 @@
 <?php
 
-// TASK: move lib functions, see below
+require 'lib_MOV.php';
+
 
 $sourceDir = 'debug/source';
 $dataDir   = 'days';
@@ -67,71 +68,6 @@ function deploy( $source, $dest, $keep )
     }
     else 
       copy("$source/$file", "$dest/$file");
-  }
-}
-
-
-// Helper
-
-/*@
-
-Returns true if a string ends with a string from a list of valid strings
-
-*/
-function filter_str_ends( $string, $valid_strings )  /*@*/  // TASK: maybe mov
-{
-  $keep = false;
-  
-  foreach( $valid_strings as $s )
-    // if( strpos($string, $s) !== false && strpos($string, $s) + strlen($s) == strlen($string))
-    if( str_ends_with( $string, $s))
-    {
-      $keep = true;
-      break;
-    }
-  
-  return $keep;
-}
-
-
-// Lib functions
-
-function backup( $sources, $backupDir, $base )  // TASK: mov
-{
-  $destDir = "$backupDir/" . date('ymd_Hi');
-
-  mkdir($destDir, 0755, true);
-  
-  foreach( $sources as $source )
-  {
-    $sub = str_replace( $base, '', $source);
-  
-    if( is_dir($source) )
-    {
-      mkdir("$destDir/$sub", 0755, true);
-      cp_recursive( $source, $destDir, $base);
-    }
-    elseif( is_file($source) )
-      copy( $source, "$destDir/$sub");
-  }
-}
-
-function cp_recursive( $dir, $destDir, $base )
-{
-  foreach( scandir($dir) as $fil )
-  {
-    if( in_array( $fil, ['.', '..']))
-      continue;
-
-    $sub = str_replace( $base, '', $dir);
-
-    if( is_dir("$dir/$fil") )
-    {
-      mkdir("$destDir/$sub/$fil", 0755, true);
-      cp_recursive("$dir/$fil", "$destD ir/$sub/$fil");
-    }
-    else
-      copy("$dir/$fil", "$destDir/$sub/$fil");
   }
 }
 
