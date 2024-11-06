b3b1fc423b6be0d063cf6ab30963ef1b76931abf
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-11-06 16:47:34 +0100

    Data input advanced

---
+++ dev/data/new.php
@@ -0,0 +1,7 @@
+<?php
+
+// https://world.openfoodfacts.org/api/v3/product/4337256080842.json
+
+// I need a PHP code that maps try json data in try.json to the blank food format. Have a look at the comments there which tell you how to map and match the data fields as good as possible. Indent all codes with 2 spaces and put the { on the next line.
+
+?>
