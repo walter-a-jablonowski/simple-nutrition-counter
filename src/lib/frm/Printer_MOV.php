fee1a596b805747060cd51ec41f45315c3642476
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-11-06 18:49:20 +0100

    Some progress with impl

---
+++ src/lib/frm/Printer_MOV.php
@@ -0,0 +1,75 @@
+<?php
+
+/*@
+
+Prints stuff
+
+file_put_contents('out.yml',  Printer::run('my.yml', [
+  'title' => 'Hello World'
+]));
+
+*/
+class Printer /*@*/
+{
+
+  public static function run( string $fil, array $data ) : string
+  {
+    ob_start();
+    require $fil;
+    return ob_get_clean();
+  }
+
+
+  /*@
+
+  Append a string
+  from damn-small-engine (append() simplified)
+
+  like print() adds space before
+
+  */
+  public static function append( $s ) /*@*/
+  {
+    // $s = self::print( $arg1, $arg2, $arg3 );
+
+    if( $s )  return " $s";
+    else      return '';
+  }
+
+
+  /*@
+
+  iif
+  from standard/dev
+
+  - valid are non-false and 0, 0.0, "0"
+
+  */
+  public static function iif( $if, $true, $false = '' ) /*@*/
+  {
+    if( $if || $if === 0 || $if === 0.0 || $if === "0" )
+      return $true;
+    else
+      return $false;
+  }
+
+  /*@
+
+  list
+
+  */
+  public static function list( $array, $item, $trim = ', ') /*@*/
+  {
+    $r = '';
+    
+    foreach( $array as $key => $value)
+    {
+      $value = ! is_bool($value) ? $value : ( $value ? 'true' : 'false');
+      $r .= str_replace('[key]', $key, str_replace('[value]', $value, $item));
+    }
+
+    return trim($r, $trim);
+  }
+}
+
+?>
