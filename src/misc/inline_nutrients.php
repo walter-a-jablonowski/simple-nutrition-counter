c78439e3e7dfbdf12e86a718cab7807a518c9e6a
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2025-02-03 21:21:57 +0100

    Minor change

--- src/misc/inline_nutrients.php
+++ src/misc/inline_nutrients.php
@@ -4,7 +4,7 @@
   <li>exact plant variety</li>
   <li>type soild where it was planted</li>
   <li>time of year, amount of sunlight</li>
-  <li>and so on ...</li>
+  <li>and so on ... (also true for meat)</li>
 </ul>
 
 <p>All of these have influence on the amount of nutrients in the food. This list uses reference data that gives you a rough approximation of the actual amount of nutrients in your food. At least this gives you an idea about your nutrient supply as good as possible. You may want to eat some less of a cetain food if a value might be too high, or use a supplement if something is far too low.</p>
