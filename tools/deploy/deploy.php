7387322c67864eb71f18694c7086c46d917095c2
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2025-05-23 20:17:46 +0200

    Fixing

--- tools/deploy/deploy.php
+++ tools/deploy/deploy.php
@@ -49,27 +49,78 @@ echo 'Done';
 
 function clear_dest( $dir, $keep )
 {
-  $keepFld = filter_str_ends( $dir, $keep );
-
-  // TASK: most likely just return
-  if( $keepFld )
-    return true;
+  global $destDir; // Access the global destDir variable
+  
+  // Check if the current directory should be kept
+  $normalized_dir = str_replace('\\', '/', $dir);
+  
+  foreach( $keep as $keep_item )
+  {
+    $keep_path = str_replace('\\', '/', "$destDir/$keep_item");
+    
+    // If this directory is or contains a path we want to keep
+    if( $normalized_dir == $keep_path || 
+        strpos($normalized_dir, $keep_path) === 0 || 
+        strpos($keep_path, $normalized_dir) === 0 )
+      return true;
+  }
+  
+  $keepFld = false;
   
   foreach( scandir($dir) as $file)
   {
     if( in_array( $file, ['.', '..']))
       continue;
-  
-    if( is_dir("$dir/$file"))
+    
+    $path = "$dir/$file";
+    $normalized_path = str_replace('\\', '/', $path);
+    
+    if( is_dir($path))
     {
-      $keepSub = clear_dest("$dir/$file", $keep);
+      $keepSub = false;
+      
+      // Check if this subdirectory should be kept
+      foreach( $keep as $keep_item )
+      {
+        $keep_path = str_replace('\\', '/', "$destDir/$keep_item");
+        
+        if( $normalized_path == $keep_path || 
+            strpos($normalized_path, $keep_path) === 0 || 
+            strpos($keep_path, $normalized_path) === 0 )
+        {
+          $keepSub = true;
+          break;
+        }
+      }
+      
+      // If not explicitly kept, check children
+      if( ! $keepSub )
+        $keepSub = clear_dest($path, $keep);
+      
       $keepFld = $keepFld || $keepSub;
       
       if( ! $keepSub )
-        rmdir("$dir/$file");
+        rmdir($path);
     }
-    elseif( is_file("$dir/$file") && ! filter_str_ends("$dir/$file", $keep ))
-      unlink("$dir/$file");  
+    elseif( is_file($path))
+    {
+      $keepFile = false;
+      
+      // Check if this file should be kept
+      foreach( $keep as $keep_item )
+      {
+        $keep_path = str_replace('\\', '/', "$destDir/$keep_item");
+        
+        if( $normalized_path == $keep_path )
+        {
+          $keepFile = true;
+          break;
+        }
+      }
+      
+      if( ! $keepFile )
+        unlink($path);
+    }
   }
 
   return $keepFld;
@@ -77,25 +128,51 @@ function clear_dest( $dir, $keep )
 
 function deploy( $source, $dest, $keep )
 {
+  global $destDir; // Access the global destDir variable
+  
   foreach( scandir($source) as $file )
   {
     if( in_array( $file, ['.', '..']))
       continue;
 
-    // Skip files/folders that are in the keep list but already exist in destination
     $dest_path = "$dest/$file";
-    if( file_exists($dest_path) && filter_str_ends($dest_path, $keep ))
+    $source_path = "$source/$file";
+    
+    // Check if this file/folder should be kept (already exists in destination)
+    $should_keep = false;
+    
+    if( file_exists($dest_path) )
+    {
+      $normalized_path = str_replace('\\', '/', $dest_path);
+      
+      foreach( $keep as $keep_item )
+      {
+        $keep_path = str_replace('\\', '/', "$destDir/$keep_item");
+        
+        // If this is a path we want to keep
+        if( $normalized_path == $keep_path || 
+            strpos($normalized_path, $keep_path) === 0 || 
+            strpos($keep_path, $normalized_path) === 0 )
+        {
+          $should_keep = true;
+          break;
+        }
+      }
+    }
+    
+    // Skip if it's a kept file/folder
+    if( $should_keep )
       continue;
 
-    if( is_dir("$source/$file"))
+    if( is_dir($source_path))
     {
       if( ! is_dir($dest_path))
         mkdir($dest_path, 0755, true);
       
-      deploy("$source/$file", $dest_path, $keep);
+      deploy($source_path, $dest_path, $keep);
     }
     else 
-      copy("$source/$file", $dest_path);
+      copy($source_path, $dest_path);
   }
 }
 
