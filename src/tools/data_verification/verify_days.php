049afb3aee6c1260c05d1cd8f7e181f206af5edd
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2025-09-25 20:25:43 +0200

    Minor change

--- src/tools/data_verification/verify_days.php
+++ src/tools/data_verification/verify_days.php
@@ -2,6 +2,7 @@
 
 // TASK: make a btn (cmd line arg) "add-unprecise" and upd the found files if header missing
 //   add_unpr_header does sth similar, maybe just prepend or use existing lib function if any
+//   low kcal day vs missing data
 
 // Day files with line counts below average
 // - Scans: src/data/users/JaneDoe@example.com-24080101000000/days
